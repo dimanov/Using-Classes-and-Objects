166fcd61660a8a62891fa7fc34f2bcf7e42754d4
dimanov <dimanov@gmail.com>
2015-02-25 17:15:05 +0200

    Day of Week

---
+++ 03. DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Problem 3. Day of week
+ * Write a program that prints to the console which day of the week is today.
+ * Use System.DateTime.
+ */
+
+using System;
+using System.Globalization;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        DateTime now = DateTime.Now;
+        var dayOfWeek = now.ToString("dddd", CultureInfo.InvariantCulture);
+        Console.WriteLine("Today is {0}", dayOfWeek);
+
+    }
+}
